e6e520a3e82ffbfc497fbedf261d9a571f50ab64
TrueMolni <truemolni@gmail.com>
2023-01-23 13:05:09 +0200

    0.0.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 import Phonebook from './Phonebook/Phonebook';
 import Contacts from './Contacts/Contacts';
+
+let id = nanoid();
+console.log(id);
+
 class App extends Component {
   state = {
     contacts: [],
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,27 +1,47 @@
+import { Component } from 'react';
 import PropTypes from 'prop-types';
-// import scss from './contacts.module.scss';
+import scss from './phonebook.module.scss';
 
-function Phonebook() {
-  return (
-    <>
-      <h2></h2>
-      <form action="">
-        <label>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
+class Phonebook extends Component {
+  state = { name: '' };
 
-        <button type="submit">Add contact</button>
-      </form>
-    </>
-  );
+  handleChange = evt => {
+    this.setState({ name: evt.target.value });
+  };
+  render() {
+    const { name } = this.state;
+    // return <input type="text" value={name} onChange={this.handleChange} />;
+    return (
+      <div className={scss.wrapper}>
+        <h2>Phonebook</h2>
+        <form action="">
+          <label>
+            Name
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="name"
+              value={name}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Phonebook;
 
-// Contacts.propTypes = {};
+// Contacts.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     }).isRequired
+//   ),
+// };
